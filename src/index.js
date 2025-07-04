277736c9b84c0cd8708bfd63661dbab43c876c10
singhaishwarya <aishsinghniit@gmail.com>
2025-07-04 18:37:57 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -6,12 +6,10 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "assets/styles/tailwind.css";
 
 // layouts
-
 import Admin from "layouts/Admin.js";
 import Auth from "layouts/Auth.js";
 
 // views without layouts
-
 import Landing from "views/Landing.js";
 import Profile from "views/Profile.js";
 import Index from "views/Index.js";
@@ -22,12 +20,19 @@ ReactDOM.render(
       {/* add routes with layouts */}
       <Route path="/admin" component={Admin} />
       <Route path="/auth" component={Auth} />
+      
+      {/* default route now points to Auth */}
+      <Route path="/" exact>
+        <Redirect to="/auth/login" />
+      </Route>
+
       {/* add routes without layouts */}
       <Route path="/landing" exact component={Landing} />
       <Route path="/profile" exact component={Profile} />
-      <Route path="/" exact component={Index} />
-      {/* add redirect for first page */}
-      <Redirect from="*" to="/" />
+      <Route path="/index" exact component={Index} />
+
+      {/* catch-all */}
+      <Redirect from="*" to="/auth/login" />
     </Switch>
   </BrowserRouter>,
   document.getElementById("root")

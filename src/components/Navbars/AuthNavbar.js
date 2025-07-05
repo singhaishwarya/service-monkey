29b837b615d3dc17d0145752cc7a436798d23a10
singhaishwarya <aishsinghniit@gmail.com>
2025-07-05 14:26:05 +0530

    text edits

--- src/components/Navbars/AuthNavbar.js
+++ src/components/Navbars/AuthNavbar.js
@@ -82,14 +82,14 @@ export default function Navbar(props) {
                 </a>
               </li>
 
-              <li className="flex items-center">
+              {/* <li className="flex items-center">
                 <button
                   className="bg-white text-blueGray-700 active:bg-blueGray-50 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
                   type="button"
                 >
                   <i className="fas fa-arrow-alt-circle-down"></i> Download
                 </button>
-              </li>
+              </li> */}
             </ul>
           </div>
         </div>
